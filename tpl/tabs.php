71da17f3a53c37290e81790f5b192ba367e3d968
gizmore <gizmore@wechall.net>
2017-12-19 18:04:27 +0100

    6.07_classic

--- tpl/tabs.php
+++ tpl/tabs.php
@@ -5,13 +5,15 @@ use GDO\UI\GDT_Bar;
 use GDO\UI\GDT_Link;
 use GDO\User\GDO_User;
 $user = GDO_User::current();
-$bar = GDT_Bar::make();
+$bar = GDT_Bar::make()->horizontal();
 $friends = GDO_Friendship::count($user);
 $incoming = GDO_FriendRequest::countIncomingFor($user);
+$link3 = GDT_Link::make('link_incoming_friend_requests')->label('link_incoming_friend_requests', [$incoming])->href(href('Friends', 'Requests'));
+if ($incoming) $link3->icon('alert');
 $bar->addFields(array(
 	GDT_Link::make('link_add_friend')->icon('add')->href(href('Friends', 'Request')),
 	GDT_Link::make('link_friends')->label('link_friends', [$friends])->icon('group')->href(href('Friends', 'FriendList')),
-	GDT_Link::make('link_incoming_friend_requests')->label('link_incoming_friend_requests', [$incoming])->icon('notifications_active')->href(href('Friends', 'Requests')),
-	GDT_Link::make('link_pending_friend_requests')->icon('notifications_paused')->href(href('Friends', 'Requesting')),
+	$link3,
+	GDT_Link::make('link_pending_friend_requests')->icon('wait')->href(href('Friends', 'Requesting')),
 ));
 echo $bar->renderCell();
