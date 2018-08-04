c3a7b45b4640d2abb841b1cbc02970df4327efb8
gizmore <gizmore@wechall.net>
2018-08-04 18:10:14 +0200

    Fix

--- Websocket/GWS_FriendsRequest.php
+++ Websocket/GWS_FriendsRequest.php
@@ -22,11 +22,21 @@ class GWS_FriendsRequest extends GWS_CommandForm
     # Map to form
     public function getMethod() { return Request::make(); }
     
+//     /**
+//      * Not needed as we hook into ws... not as nice but meh. Correct would be only IPC hook. Now only websocket. no other hook into this yet
+//      */
+//     public function hookFriendsRequest($id)
+//     {
+//         $request = GDO_FriendRequest::findByGID($id);
+//         $this->hookFriendsRequestB($request);
+//     }
+    
+    
     /**
      * Hook friend requests and notify target.
      * @param int $requestId
      */
-    public function hookFriendsRequest(GDO_FriendRequest $request)
+    public function hookFriendsRequestB(GDO_FriendRequest $request)
     {
         $friend = $request->getFriend();
         $payload = GWS_Message::payload(0x0601);
@@ -37,7 +47,7 @@ class GWS_FriendsRequest extends GWS_CommandForm
     public function postExecute(GWS_Message $msg, GDT_Form $form, GDT_Response $response)
     {
         $request = GDO_FriendRequest::getById($msg->user()->getID(), $form->getFormVar('frq_friend'));
-        $this->hookFriendsRequest($request);
+        $this->hookFriendsRequestB($request);
     }
 }
 
