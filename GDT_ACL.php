d2cbb576db574c03db5263723d5c9d6bb37173c3
gizmore <gizmore@wechall.net>
2018-09-09 02:03:30 +0200

    6.08-r38_install_fixes

--- GDT_ACL.php
+++ GDT_ACL.php
@@ -2,8 +2,64 @@
 namespace GDO\Friends;
 
 use GDO\DB\GDT_Enum;
+use GDO\User\GDO_User;
+use GDO\Core\GDOException;
+use GDO\DB\Query;
 
+/**
+ * An ACL field has default ACL options.
+ * It helps to construct queries to reflect ACL permission.
+ * 
+ * @author gizmore@wechall.net
+ * @version 6.08
+ * @since 6.08
+ */
 final class GDT_ACL extends GDT_Enum
 {
+	public function __construct()
+	{
+		$this->enumValues = ['acl_all', 'acl_members', 'acl_friends', 'acl_noone'];
+		$this->initial = 'acl_noone';
+	}
+	
+	public function hasAccess(GDO_User $user, GDO_User $target)
+	{
+		if ($user === $target) { return true; }
+		switch ($this->initial)
+		{
+			case 'acl_all'; return true;
+			case 'acl_members'; return $user->isMember();
+			case 'acl_friends'; return module_enabled('Friends') ? GDO_Friendship::areRelated($user, $target) : false;
+			case 'acl_noone'; return false;
+			default: throw new GDOException('ACL enum value is invalid: '.$this->initial);
+		}
+	}
+	
+	public function aclQuery(Query $query, GDO_User $user, $creatorColumn)
+	{
+		# All
+		$idf = $this->identifier();
+		$condition = "$idf = 'acl_all'";
+
+		# Members
+		if ($user->isMember())
+		{
+			$condition .= " OR $idf = 'acl_members'";
+		}
+		
+		# Friends and own
+		if ($creatorColumn)
+		{
+			$uid = $user->getID();
+			$condition .= " OR $creatorColumn = {$uid}";
+			if (module_enabled('Friends'))
+			{
+				$subquery = "SELECT 1 FROM gdo_friendship WHERE friend_user=$uid AND friend_friend=$creatorColumn";
+				$condition .= " OR ( $idf = 'acl_friends' AND ( $subquery ) )";
+			}
+		}
+		$query->where($condition);
+		return $this;
+	}
 	
 }
