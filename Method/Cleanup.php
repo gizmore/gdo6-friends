30a0fad9b6397a1844ca84bd9b72066917ccc06a
gizmore <gizmore@wechall.net>
2020-09-21 10:30:36 +0200

    6.10_r9004_time_and_dog

--- Method/Cleanup.php
+++ Method/Cleanup.php
@@ -5,6 +5,7 @@ use GDO\DB\Database;
 use GDO\Date\Time;
 use GDO\Friends\GDO_FriendRequest;
 use GDO\Friends\Module_Friends;
+use GDO\Core\Application;
 use GDO\Cronjob\MethodCronjob;
 
 final class Cleanup extends MethodCronjob
@@ -12,7 +13,7 @@ final class Cleanup extends MethodCronjob
 	public function run()
 	{
 		$module = Module_Friends::instance();
-		$cut = Time::getDate(time() - $module->cfgCleanupAge());
+		$cut = Time::getDate(Application::$TIME - $module->cfgCleanupAge());
 		GDO_FriendRequest::table()->deleteWhere("frq_denied < '$cut'");
 		if ($affected = Database::instance()->affectedRows())
 		{
